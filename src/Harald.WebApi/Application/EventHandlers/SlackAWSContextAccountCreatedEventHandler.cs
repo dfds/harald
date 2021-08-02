b3c021d7d5ef658061bd7a3b8dfbf77de7ef66e2
Peter West <pwes@dfds.com>
2021-08-02 14:49:58 +0100

    Modified Slack message to reflect new pipeline.

--- src/Harald.WebApi/Application/EventHandlers/SlackAWSContextAccountCreatedEventHandler.cs
+++ src/Harald.WebApi/Application/EventHandlers/SlackAWSContextAccountCreatedEventHandler.cs
@@ -22,14 +22,8 @@ namespace Harald.WebApi.Application.EventHandlers
         {
             var addUserCmd =
                 $"Get-ADUser \"CN=IT BuildSource DevEx,OU=DFDS AS,OU=Mailboxes,OU=Accounts,OU=DFDS,DC=dk,DC=dfds,DC=root\" | Set-ADUser -Add @{{proxyAddresses=\"smtp:{domainEvent.Payload.RoleEmail}\"}}";
-            var installToolsCmd =
-                $"Get-WindowsCapability -Online | ? {{$_.Name -like 'Rsat.ActiveDirectory.DS-LDS.Tools*'}} | Add-WindowsCapability -Online";
-            var addDeployCredentialsBash = $"AWS_ROLE_CLOUD_ADMIN=\"[Cloud Administrator Role ARN]\"\\\n" +
-                                           $"AWS_ROLE_ADFS_ADMIN=\"[ADFS Administrator Role ARN]\"\\\n" +
-                                           $"poetry run python ./kube_config_generator.py -r {domainEvent.Payload.CapabilityRootId} -a {domainEvent.Payload.AccountId}";
-            var addDeployCredentialsPS = $"$ENV:AWS_ROLE_CLOUD_ADMIN=\"[Cloud Administrator Role ARN]\"\\\n" +
-                                         $"$ENV:AWS_ROLE_ADFS_ADMIN=\"[ADFS Administrator Role ARN]\"\\\n" +
-                                         $"poetry run python .\\kube_config_generator.py -r {domainEvent.Payload.CapabilityRootId} -a {domainEvent.Payload.AccountId}";
+            var pipelineVariableValues = $"AccountID: {domainEvent.Payload.AccountId}\\\n" +
+                                         $"RootID: {domainEvent.Payload.CapabilityRootId}\\\n";
 
             // poetry run python kube_config_generator.py -r {domainEvent.Payload.CapabilityRootId} -a {domainEvent.Payload.AccountId}
             var sb = new StringBuilder();
@@ -38,12 +32,9 @@ namespace Harald.WebApi.Application.EventHandlers
             sb.AppendLine("\n_Add email address to shared mailbox_");
             sb.AppendLine("Execute the following Powershell command:");
             sb.AppendLine($"`{addUserCmd}`");
-            // sb.AppendLine($"Should you not have RSAT tools installed, please do so with command:");
-            // sb.AppendLine(installToolsCmd);
             sb.AppendLine($"\n_Generate k8s service account_");
-            sb.AppendLine($"Execute the Python script from github.com/dfds/ce-toolbox/k8s-service-account-config-to-ssm.  Please ensure that the two environment variables are modified to include the correct Role ARNs.");
-            sb.AppendLine($"Bash:\n```{addDeployCredentialsBash}```");
-            sb.AppendLine($"Powershell:\n```{addDeployCredentialsPS}```");
+            sb.AppendLine($"Run the Azure DevOps Pipeline **k8s-service-account-config-to-ssm**.\nPlease ensure you provide values for the four variables attached to the pipeline.");
+            sb.AppendLine($"```{pipelineVariableValues}```");
 
             var hardCodedDedChannelId = new ChannelId("GFYE9B99Q");
             await _slackFacade.SendNotificationToChannel(hardCodedDedChannelId.ToString(), sb.ToString());
